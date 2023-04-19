0ab990c3f139a092b7b1044196c07342edbbf085
superkidsyuvan <meetnandhini@outlook.com>
2023-04-19 15:55:37 -0500

    aa

--- src_files/data/add_on_library.js
+++ src_files/data/add_on_library.js
@@ -1,3 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width:550,
+        height:325,
+        icon: "./resources/app/src_files/images/bin/SYS/ico/light.ico",
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('./require/additions/dog.html')
+    win.setMenuBarVisibility(false)
+
+    //win.webContents.openDevTools()
+    //  <meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-inline';"> <script src="window.location.href.slice(0,-10)+'add_in_library.js'"> <-- tag use in head if no work (:
+    // also add in body LAST, <div id="library_com"></div> (also <div id="sound_lib"></div>)
+    // add bootleg() to end of imgur() and google()
+    // add bootarm() to end of RBX() and drive()
+    // remeber the button sound thing idk i forgor 
+}
+
+
+app.on('window-all-closed', ()=>{
+    if(process.platform!=='darwin'){
+        app.quit()
+    }
+})
+
 function desktop(){
     IMAGEserver="desktop"
     document.getElementById("google").innerHTML="Google Photos (Recommended for school computers)"
@@ -17,6 +47,10 @@ function desktopSOUND(){
 
 function bootleg(){
     document.getElementById("desktop").innerHTML="Desktop (no internet needed)"
+}
+
+function openTTW(){
+    createWindow()
 }
 
 function bootarm(){
